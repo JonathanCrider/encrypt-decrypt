326d03dbda58857edea67801f886cf34490fdfea
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-01-06 18:38:51 -0500

    fix if statements

--- install.sh
+++ install.sh
@@ -9,22 +9,23 @@ SCRIPT_URL="https://raw.githubusercontent.com/JonathanCrider/encrypt-decrypt/mai
 mkdir -p "$TARGET_DIR"
 
 # Check if the script already exists
-# if [[ -f "$SCRIPT_PATH" ]]
-  # then
-  #   echo "The script '$SCRIPT_NAME' already exists in $TARGET_DIR."
-  #   echo "Do you want to overwrite it? [y/N]: "
-  #   read -r "response"
-  #   if [[ "$response" != "y" && "$response" != "Y" ]]
-  #     then
-  #       echo "Installation canceled."
-  #       exit 0
-  #   fi
-  #   rm $SCRIPT_PATH
-# fi
+if [ -f "$SCRIPT_PATH" ]
+  then
+    echo "The script '$SCRIPT_NAME' already exists in $TARGET_DIR."
+    echo "Do you want to overwrite it? [y/N]: "
+    read -r "response"
+    if [[ "$response" != "y" && "$response" != "Y" ]]
+      then
+        echo "Installation canceled."
+        exit 0
+    fi
+    rm $SCRIPT_PATH
+fi
 
 # Download source file and give relevant permissions
 echo "downloading script $SCRIPT_NAME"
-if curl -o "$SCRIPT_PATH" -sSl "$SCRIPT_URL"
+
+if [[ curl -o "$SCRIPT_PATH" -sSl "$SCRIPT_URL" ]]
   then
     chmod +x "$SCRIPT_PATH"
     echo "Downloaded and installed '$SCRIPT_NAME' into '$SCRIPT_PATH'"
@@ -37,7 +38,7 @@ fi
 SHELL_CONFIG="$HOME/.zshrc"
 SOURCE_CMD="source $SCRIPT_PATH"
 
-if ! grep -q "$SOURCE_CMD" "$SHELL_CONFIG"
+if [[ ! grep -q "$SOURCE_CMD" "$SHELL_CONFIG" ]]
   then
     echo "\n# Source encrypt/decrypt functions\n$SOURCE_CMD" >> "$SHELL_CONFIG"
     echo "Added '$SOURCE_CMD' to $SHELL_CONFIG."
