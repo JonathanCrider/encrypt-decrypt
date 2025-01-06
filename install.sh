b971d2e2be57db65b05afb5f0f3e593a0fe349c1
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-01-06 18:01:22 -0500

    add some error handling

--- install.sh
+++ install.sh
@@ -1,22 +1,47 @@
 #!/bin/zsh
 
 TARGET_DIR="${HOME}/.local/bin"
-SCRIPT_URL="https://raw.githubusercontent.com/JonathanCrider/encrypt-decrypt/main/encrypt-decrypt"
+SCRIPT_NAME="encrypt-decrypt"
+SCRIPT_PATH="$TARGET_DIR/$SCRIPT_NAME"
+SCRIPT_URL="https://raw.githubusercontent.com/JonathanCrider/encrypt-decrypt/main/$SCRIPT_NAME"
 
 # Create directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
+# Check if the script already exists
+if [[ -f "$SCRIPT_PATH" ]]
+  then
+    echo "The script '$SCRIPT_NAME' already exists in $TARGET_DIR."
+    echo "Do you want to overwrite it? [y/N]: "
+    read -r response
+    if [[ "$response" != "y" && "$response" != "Y" ]]
+      then
+        echo "Installation canceled."
+        exit 0
+      else
+        rm -rf $SCRIPT_PATH
+    fi
+fi
+
 # Download source file and give relevant permissions
-curl -o "$TARGET_DIR/encrypt-decrypt" "$SCRIPT_URL"
-chmod +x "$TARGET_DIR/encrypt-decrypt"
+echo "downloading script $SCRIPT_NAME"
+if curl -o "$SCRIPT_PATH" -sSl "$SCRIPT_URL"
+  then
+    chmod +x "$SCRIPT_PATH"
+    echo "Downloaded and installed '$SCRIPT_NAME' into '$SCRIPT_PATH'"
+  else
+    echo "Failed to download '$SCRIPT_NAME'. Please check your internet connection or the URL"
+    exit 1
+fi
 
 # Add sourcing to .zshrc
 SHELL_CONFIG="$HOME/.zshrc"
+SOURCE_CMD="source $SCRIPT_PATH"
 
-if ! grep -q "source $TARGET_DIR/encrypt-decrypt" "$SHELL_CONFIG"
+if ! grep -qF "$SOURCE_CMD" "$SHELL_CONFIG"
   then
-    echo "\n# Source encrypt/decrypt functions\nsource $TARGET_DIR/encrypt-decrypt" >> "$SHELL_CONFIG"
-    echo "Added 'source $TARGET_DIR/encrypt-decrypt' to $SHELL_CONFIG."
+    echo "\n# Source encrypt/decrypt functions\n$SOURCE_CMD" >> "$SHELL_CONFIG"
+    echo "Added '$SOURCE_CMD' to $SHELL_CONFIG."
   else
     echo "Something went wrong, please try again"
     return 1
