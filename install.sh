e3fd9d03a27cb5f818a68a251b2191fcd91372ec
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-01-06 18:22:20 -0500

    fix response

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ fi
 SHELL_CONFIG="$HOME/.zshrc"
 SOURCE_CMD="source $SCRIPT_PATH"
 
-if ! grep -qF "$SOURCE_CMD" "$SHELL_CONFIG"
+if ! grep -q "$SOURCE_CMD" "$SHELL_CONFIG"
   then
     echo "\n# Source encrypt/decrypt functions\n$SOURCE_CMD" >> "$SHELL_CONFIG"
     echo "Added '$SOURCE_CMD' to $SHELL_CONFIG."
