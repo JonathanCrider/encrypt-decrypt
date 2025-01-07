107694fbbc460b7fe8d55f481a6d4b3963f3b0b6
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-01-06 21:15:58 -0500

    fix source line removal

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
+# Globals
 TARGET_DIR="${HOME}/.local/bin"
 SCRIPT_NAME="encrypt-decrypt"
 SCRIPT_PATH="$TARGET_DIR/$SCRIPT_NAME"
 SHELL_CONFIG="$HOME/.zshrc"
+SOURCE_CMD="source $SCRIPT_PATH"
 
 # Remove the script
 if [[ -f "$SCRIPT_PATH" ]]; then
@@ -14,9 +16,13 @@ else
 fi
 
 # Remove the source line from .zshrc
-if grep -qF "source $SCRIPT_PATH" "$SHELL_CONFIG"
+if grep -qF "$SOURCE_CMD" "$SHELL_CONFIG"
   then
-    sed -i '' "/source $SCRIPT_PATH/d" "$SHELL_CONFIG"
+    # for sed, you must change the delimiter when removing a line with
+    # slashes, such as a path. Escape the new delimiter once at the beginning
+    # of the line, i.e. "\~your/path/here~d" to use ~ in place of the usual /
+    sed -i '' "\~# Source encrypt/decrypt functions~d" "$SHELL_CONFIG"
+    sed -i '' "\~$SOURCE_CMD~d" "$SHELL_CONFIG"
     echo "Removed sourcing line from $SHELL_CONFIG."
   else
     echo "No sourcing line found in $SHELL_CONFIG."
